3210b9c47254377d7aafc65a6919416b3c44147f
Robert9087 <148253313+Robert9087@users.noreply.github.com>
2025-10-27 17:07:30 +0100

    board was created, without any physics

---
+++ tetris.js
@@ -0,0 +1,36 @@
+const height = 16
+const width = 10
+
+window.onload = function() {
+    board();
+}
+
+
+function board() {
+    part = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+    for (let i = 2; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            let unit = document.createElement("div");
+            unit.id = i.toString() + "-" + j.toString();
+            unit.classList.value = "tiles";
+            document.getElementById("board").append(unit);
+        }
+    }
+}
